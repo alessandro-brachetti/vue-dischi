80e81dbb437fbe720926a87799b19bc7235526da
Alessandro Brachetti <alebrack13@gmail.com>
2021-04-09 18:08:18 +0200

    Esercizio Completo + Bonus 1 e 2

--- main.js
+++ main.js
@@ -4,13 +4,31 @@ var app = new Vue ({
   el: '#root',
   data: {
     disks: [],
+    genres: [],
+    genre: '',
   },
   mounted:function () {
     axios.get('https://flynn.boolean.careers/exercises/api/array/music')
       .then((response) => {
         this.disks = response.data.response;
         console.log(this.disks);
+
+        this.disks.forEach(disk => {
+          if(!this.genres.includes(disk.genre)) {
+          this.genres.push(disk.genre);
+          }
+        });
       });
+  },
+  computed: {
+    filteredDisks: function () {
+      return this.disks.filter(disk => !disk.genre.indexOf(this.genre))
+    }
+  },
+  methods: {
+    sorted() {
+      this.disks.sort((a,b) => a.year > b.year ? 1 : -1);
+    }
   },
 
 })
